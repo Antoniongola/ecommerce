61fb660b3fa19fef39bff6785c3c6527642494f0
antoniongola <antoniongola10@gmail.com>
2024-12-11 14:10:08 +0100

    feat: adding the product entity.

---
+++ src/main/java/com/ngolajr/ecommerce/model/Product.java
@@ -0,0 +1,21 @@
+package com.ngolajr.ecommerce.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private double price;
+    private int quantity;
+    private String description;
+    private String image;
+}
